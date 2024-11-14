ef5151aa7e170a173f36ade5a05a2a8b00b84905
唐菊 <635465547@qq.com>
2024-11-14 16:22:15 +0800

    将请求最大的数据改为50M, 默认为100kb

--- app.ts
+++ app.ts
@@ -22,8 +22,8 @@ setRouteConvention({
 });
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: false, limit: '50mb' }));
 app.use(cookieParser());
 //app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));

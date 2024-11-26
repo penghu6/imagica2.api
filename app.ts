1523ed526cbf8cbb21853aa833175a65755b60e7
orinly <1019836476@qq.com>
2024-11-26 14:11:16 +0800

    update

--- app.ts
+++ app.ts
@@ -36,7 +36,7 @@ app.use(logger('dev'));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: false, limit: '50mb' }));
 app.use(cookieParser());
-app.use(cors());
+// app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 
 (async () => {

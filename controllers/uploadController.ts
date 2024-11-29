534e15bbf9269371de1770d6b415cbb6e55171ca
唐菊 <635465547@qq.com>
2024-11-29 16:27:05 +0800

    Upload zip file and unzip (#31)
    * 修复上传后前端拿不到结果的问题，并且修改报错不返回
    
    * 上传文件解压到文件夹中
    
    * 修改api文档

--- controllers/uploadController.ts
+++ controllers/uploadController.ts
@@ -6,6 +6,7 @@ import path from 'path';
 import { UploadError } from '../utils/errors';
 import { formatResponse } from '../utils/tools';
 import { createDatePath, ensureDir, getAccessPath } from '../utils/fileHelper';
+import UploadService from '../services/uploadService';
 
 interface MulterRequest extends Request {
   file: Express.Multer.File;
@@ -19,6 +20,12 @@ interface MulterRequest extends Request {
  */
 @Controller('upload')
 export class UploadController extends BaseController {
+  private uploadService: UploadService
+  constructor() {
+    super();
+    this.uploadService = new UploadService();
+}
+
   private uploader = multer({
     limits: {
       fileSize: 50 * 1024 * 1024
@@ -42,7 +49,7 @@ export class UploadController extends BaseController {
    * /api/upload:
    *   post:
    *     summary: 上传文件
-   *     description: 支持上传单个文件，大小限制为50MB
+   *     description: 支持上传.zip，大小限制为50MB
    *     tags: [Upload]
    *     consumes:
    *       - multipart/form-data
@@ -75,8 +82,8 @@ export class UploadController extends BaseController {
    *                   description: 提示信息
    *                 data:
    *                   type: string
-   *                   example: "/static/uploads/20240101120000/1234567890-file.jpg"
-   *                   description: 文件访问路径
+   *                   example: "/static/uploads/20240101120000"
+   *                   description: 压缩文件解压后的文件夹路径
    *       400:
    *         description: 上传失败
    *         content:
@@ -93,17 +100,31 @@ export class UploadController extends BaseController {
    */
   @Post('/')
   async uploadFile(req: Request, res: Response, next: NextFunction) {
-    this.uploader.single("file")(req, res, (err: any) => {
-      if (err instanceof multer.MulterError) {
-        next(new UploadError("上传文件失败，请检查文件的大小，控制在 50MB 以内"));
-      } else if (err) {
-        next(new UploadError("上传文件失败"));
-      } else {
-        const file = (req as MulterRequest).file;
-        const basePath = process.env.FILE_PATH || './public';
-        const accessPath = getAccessPath(file.path, basePath);
-        res.send(formatResponse(0, "", accessPath));
-      }
-    });
+    try {
+      const filePath = await new Promise<string>((resolve, reject) => {
+        this.uploader.single("file")(req, res, async (err: any) => {
+          if (err instanceof multer.MulterError) {
+            reject(new UploadError("上传文件失败，请检查文件的大小，控制在 50MB 以内"));
+          } else if (err || !req || !(req as MulterRequest).file) {
+            reject(new UploadError("上传文件失败"));
+          } else{
+            const file = (req as MulterRequest).file;
+            const basePath = process.env.FILE_PATH || './public';
+            const accessPath = getAccessPath(file.path, basePath);
+            try {
+              await this.uploadService.unzipFiles(file.path)
+              resolve(accessPath);
+            } catch (error: any) {
+              res.status(400).send(formatResponse(1, error.message))
+            }
+            
+          }
+        });
+      });
+      const outputDir = path.dirname(filePath)
+      res.send(formatResponse(0, "", outputDir));
+    } catch (error: any) {
+      return formatResponse(1, error.message);
+    }
   }
 }

298c3738ace47bc037aa4064b42c6f901030a1f4
唐菊 <635465547@qq.com>
2024-12-25 11:31:38 +0800

    modify react template

--- imagica2/bucket/templates/react-template/src/App.tsx
+++ imagica2/bucket/templates/react-template/src/App.tsx
@@ -1,24 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <h1 className="text-4xl font-bold bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 text-transparent bg-clip-text">
+        Welcome to Imagica
+      </h1>
     </div>
   );
 }

bff850bd0d43c2820aeb99bcc1d4811707382a68
唐菊 <635465547@qq.com>
2024-12-25 15:47:40 +0800

    Modify vue template (#53)
    * modify vue template
    
    * 去掉部分检查

--- imagica2/bucket/templates/vue-template/src/components/HelloWorld.vue
+++ imagica2/bucket/templates/vue-template/src/components/HelloWorld.vue
@@ -1,41 +1,11 @@
 <script setup lang="ts">
-defineProps<{
-  msg: string
-}>()
+const msg = 'Welcome to Imagica'
 </script>
 
 <template>
-  <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a href="https://vite.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
-    </h3>
+  <div class="text-center">
+    <h1 class="text-4xl font-bold bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 bg-clip-text text-transparent">
+      {{ msg }}
+    </h1>
   </div>
 </template>
-
-<style scoped>
-h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
-  position: relative;
-  top: -10px;
-}
-
-h3 {
-  font-size: 1.2rem;
-}
-
-.greetings h1,
-.greetings h3 {
-  text-align: center;
-}
-
-@media (min-width: 1024px) {
-  .greetings h1,
-  .greetings h3 {
-    text-align: left;
-  }
-}
-</style>
